540a52190abe88605f987a0daf468a6518546b5b
Leslie Rodriguez <rleslie1015@gmail.com>
2020-08-22 01:42:21 -0500

    started algo has bugs with counter and counting neighbor function

--- life/src/App.js
+++ life/src/App.js
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 //components
 import Board from './Board';
 import Rules from './Rules';
+// preset data
+import {squares1} from './presets';
+
 // material ui
 import {
   Container,
@@ -42,42 +45,128 @@ import {
  function App() {
   const classes = useStyles();
   // state
-  const [gen, setGen] = useState(1);
+  const [gen, setGen] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
-  const [squares, setSquares] = useState(() => {
-    return  Array.from({length: 25}).map(() => Array.from({length: 25}).fill(0))
+  // the following constructs a two dimensional array 
+  // 25 arrays with length of 25 and every value in each array is 0
+  const [squares, setSquares] = useState(() => { // using an arrow function so this only renders once
+    return Array.from({length: 25}).map(() => Array.from({length: 25}).fill(0))
   })
-  // const [squares, setSquares] = useState(() => {
-  //   const rows = []
-  //   for (let i = 0; i < 25; i++){
-  //     rows.push(Array.from(Array(25), () => 0))
-  //   } 
-  //   return rows
-  // })
-  console.log(squares)
+  
+
+  // function for counting neighbors takes in the index of row and column
+  const countLiveNeibors = (r, c) => {
+    // for squares[i][j] example squares[1][1] the neighbors are
+    // top neibors 
+      // 0, 0 ==== squares[i-1][j-1]
+      // 0, 1 ==== squares[i-1][j-0]
+      // 0, 2 ==== squares[i-1][j+1]
+
+    // side neibors
+      // 1, 0 ==== squares[i-0][j-1]
+      // 1, 2 ==== squares[i-0][j+1]
+
+    // bottom neibors
+      // 2, 0 ==== squares[i+1][j-1]
+      // 2, 1 ==== squares[i+1][j-0]
+      // 2, 2 ==== squares[i+1][j+1]
+
+      // you can get all neibors by adding or subtracting 0, -1, or 1 each time 
+      // 0, 0 -> 0 + 0 and 0 + 1 == 0, 1
+
+    // set a variable for the count 
+    let liveCount = 0;
+
+    // how to check each neibor
+    // we have i and j 
+    // we need to change i with -1, 0 and 1
+    //  we need to change i with -1, 0 and 1
+
+    // ?????????????????????????????????????????????????????????????????????????????????????????????????
+
+       
+    // return the count
+    return liveCount;
+  }
+
+  const runSimulation = () => {
+    // update generation
+    setGen(gen + 1)
+
+    // if simulation is stopped return
+    if (isRunning == false) {
+      return;
+    }
+
+    // loop over each square checking for the status of live neibors
+    squares.forEach((row, i) =>{
+      row.forEach((column, j) =>{
+
+        // check neibors
+        let neighbors = countLiveNeibors(i, j)
+        // console.log(neighbors)
+        // if/else statement --> if for if square is alive and else for if square is dead
+        if (squares[i][j]) {
+          // if 1 or no neighbors or if more than 4 neibors
+          if (neighbors <= 1 || neighbors >= 4){
+            // it dies
+            squares[i][j] = 0
+          }
+        } else {
+          // if the perfect amount of neighbors
+          if (neighbors == 3) {
+            // dead square comes alive 
+            squares[i][j] = 1
+          }
+        }
+      })
+    })
+    // recursively call the function again every second
+    setTimeout(runSimulation, 1000);
+  }
+
+  const startSimulation = () => {
+    // sets running state
+    setIsRunning(true)
+    // call runSimulation() to start the algo
+    runSimulation()
+  }
+
+  const stopSimulation = () => {
+    // sets running state
+    setIsRunning(false)
+  }
+
+  const preset1 = () => {
+    // sets the square state with presets imported from presets file
+    setSquares([...squares1])
+  }
+  
   return (
     <Container>
       <header>
         <Typography variant='h1' align='center'>Game of Life</Typography>
       </header>
       <Grid container>
-        <Grid item xs={4} className={classes.rulesSection}>
+        <Grid item xs={12} md={4} className={classes.rulesSection}>
           <Rules/>
         </Grid>
 
-        <Grid item xs={7} className={classes.board}>
-            <Typography>Generation: 0</Typography>
+        <Grid item xs={12} md={7} className={classes.board}>
+            <Typography>Generation: {gen} </Typography>
             <Board squares={squares} setSquares={setSquares} />
         </Grid>
 
         {/* Game controls */}
         <Grid item className={classes.controlSection}>
           <ButtonGroup className={classes.controls} color="primary" variant="contained" aria-label="Buttons for controlling game of life">
-            <Button>Start</Button>
-            <Button>Stop</Button>
+            <Button onClick={startSimulation}>Start</Button>
+            <Button onClick={stopSimulation}>Stop</Button>
             <Button>Reset</Button>
           </ButtonGroup>
-          
+          <ButtonGroup className={classes.controls} color="primary" variant="contained" aria-label="Buttons for setting starting point with preset squares">
+            <Button onClick={preset1}>Preset 1</Button>
+          </ButtonGroup>
         </Grid>
       </Grid>
     </Container>
--- life/src/Board.js
+++ life/src/Board.js
@@ -1,52 +1,62 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from "react";
 //material ui
-import { Grid } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
-    container: {
-        width: 500,
-    },
-    gridSquare: {
-        width: 20,
-        height: 20,
-        boxSizing:'border-box',
-        border: '1px solid grey',
-    }
-})
-const Board = ({squares, setSquares }) => {
-    const classes = useStyles();
-    
+  container: {
+    maxWidth: 500,
+  },
+  gridSquare: {
+    width: 20,
+    height: 20,
+    boxSizing: "border-box",
+    border: "1px solid grey",
+  },
+});
+const Board = ({ squares, setSquares }) => {
+  const classes = useStyles();
 
-    useEffect(() => {
-
-    }, [squares])
-
-    return (
-        <Grid container className={classes.container}>
-            {
-                squares.map((rows, i)=> 
-                    rows.map((column, k) => <Grid item
-                                                key={`${i}-${k}`}
-                                                row={i} 
-                                                column={k} 
-                                                className={classes.gridSquare} 
-                                                onClick={(event) => {
-                    
-                                                    const updated = squares[i].splice([k], 1, 1)
-                                                    setSquares([...squares])
-                                                    // console.log('up', updated)
-                                                    // setSquares()
-                                                    // setSquares([ ...squares, squares[i][k] = 1 ] )
-                                                    // squares[i][k] = 1
-                                                }}
-                                                style={{
-                                                    backgroundColor: squares[i][k] == 0 ? null : 'red'
-                                                }}
-                                                />))
-            }
-        </Grid>
-    )
-}
+  return (
+    <Grid container className={classes.container}>
+      { // outter map loops through the rows 
+        // inner map loops through the columns 
+        //this creates the grid
+      squares.map((rows, i) =>
+        rows.map((column, j) => (
+          <Grid
+            item
+            key={`${i}-${j}`}
+            row={i}
+            column={j}
+            className={classes.gridSquare}
+            // the nested array gets updated in the onClick function for each square and updates state with setSquares()
+            onClick={() => {
+                // console.log(i,j)
+                // to toggle a square we target it like --> squares[i][k]
+                // the splice method gets called on the array `squares[i]` EX: squares[0] is the complete first row
+                // the first param [k] points to index [k] on that row. 
+                // the second is the number of elements(squares) we want to replace/delete 
+                // the last is the value we want at that square 
+            
+              squares[i][j] == 0 
+              ? 
+              squares[i].splice([j], 1, 1) // delete the value of squares[i][k] (0) and replace with 1
+              : 
+              squares[i].splice([j], 1, 0) // delete the value of squares[i][k] (0) and replace with 0
+              
+              setSquares([...squares]); // update state 
+     
+            }}
+            // set the background color of the square depending on the value 
+            style={{
+              backgroundColor: squares[i][j] == 0 ? null : "red",
+            }}
+          />
+        ))
+      )}
+    </Grid>
+  );
+};
 
 export default Board;
